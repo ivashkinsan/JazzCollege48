e5ed4cbcf01b1fbe7a1c1827c090379da6b5b6ad
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2026-04-13 22:48:53 +0300

    123

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JazzCollege48/',
   plugins: [react()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
