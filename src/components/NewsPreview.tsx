069cd12a9d2d5d714fec0061f95befe87dce225d
jazzscriptdev <ivashkinsan@gmail.com>
2026-07-06 10:37:59 +0300

    bug_fix

--- src/components/NewsPreview.tsx
+++ src/components/NewsPreview.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import type { ExtendedNewsItem } from '../types/college';
-import { Link } from 'react-router-dom';
 import Lightbox from './Lightbox';
 import styles from './NewsPreview.module.css';
 
@@ -141,9 +140,9 @@ function NewsPreview({ news }: NewsPreviewProps) {
         </div>
 
         <div className={styles.moreLink}>
-          <Link to="/news" className={styles.moreButton}>
+          <div className={styles.moreButton}>
             Все новости →
-          </Link>
+          </div>
         </div>
       </div>
 
